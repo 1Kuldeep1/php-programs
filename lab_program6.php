5df683da54f8f298e0290743e1f4d3c86266fefa
1Kuldeep1 <131331564+1Kuldeep1@users.noreply.github.com>
2023-05-02 10:42:46 +0530

    Add files via upload

---
+++ lab_program6.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> 
+<?php 
+echo "<h1> refresh page</h1>"; 
+$file = 'count.txt'; 
+//to collect the data from $file
+$c = file_get_contents($file);
+//to 
+file_put_contents($file,$c+1); 
+echo "the number of users visited :".$c; 
+?> 
+</body>
+</html>
---
+++ lab_program7.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+    <meta http-equiv="refresh" content="1"/>
+    <style>
+        p{
+            color:yellow;
+            font-size: 90px;
+            position: absolute;
+            top: 40%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+        }
+        body{
+            background-color: maroon;
+        }
+    </style>
+    <p><?php echo date("h:1:sA");?></p>
+
+</html>
